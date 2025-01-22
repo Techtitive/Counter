4ede10374141d8615646a6a0ab43dd3e7159b2a2
Techtitive <151868617+Techtitive@users.noreply.github.com>
2025-01-22 20:52:03 +0530

    Update Action.js

--- Action.js
+++ Action.js
@@ -64,10 +64,16 @@ hovertarget.addEventListener('mouseleave', () => {
 
 hovertarget.addEventListener('mouseenter', () => {
     countertext.style.color = 'white';
-    title.style.color = 'white';
 })
 
 hovertarget.addEventListener('mouseleave', () => {
     countertext.style.color = 'black';
+})
+
+hovertarget.addEventListener('mouseenter', () => {
+    title.style.color = 'white';
+})
+
+hovertarget.addEventListener('mouseleave', () => {
     title.style.color = 'black';
 })
